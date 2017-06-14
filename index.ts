81d0e213f4e181387945bdaefbc10650dba5e92d
Graham <graham.gibbons@gmail.com>
2017-06-14 13:09:06 -0700

    Rename

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import {
 	createViewer,
-	entity,
+	model,
 	material,
 	mouseCamera,
 	scale,
@@ -18,19 +18,19 @@ createViewer(document.body, scene({
 		aperture: value(0.01)
 	}),
 	entities: [
-		entity(
+		model(
 			translate(value(0, 20, 0),
 				scale(value(2), unitBox())),
 			material({
 				emissivity: value(8, 8, 8)
 			})),
-		entity(
+		model(
 			translate(value(0, 10, 0),
 				scale(value(10), unitBox())),
 			material({
 				color: value(0.8, 0.8, 0.8)
 			})),
-		entity(
+		model(
 			translate(value(0, 2, -4),
 				unitSphere()),
 			material({
