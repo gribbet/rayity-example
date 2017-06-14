9a8d50fca1c0d88e0e340acc418b646cd7d162db
Graham <graham.gibbons@gmail.com>
2017-06-13 17:04:36 -0700

    Update example

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-import {createViewer, entity, material, plane, scale, scene, translate, unitBox, unitSphere, value} from "traymarch";
+import {
+	createViewer,
+	entity,
+	material,
+	mouseCamera,
+	plane,
+	scale,
+	scene,
+	translate,
+	unitBox,
+	unitSphere,
+	value
+} from "traymarch";
 
-createViewer(document.body, scene([
-	entity(
-		unitSphere(),
-		material({
-			color: value(0.8, 0.8, 1)
-		})),
-	entity(
-		translate(value(0, 0, 4),
-			scale(value(5), unitBox())),
-		material({
-			color: value(0.8, 0.8, 0.8)
-		})),
-	entity(
-		plane(value(0, 0, -1), value(5)),
-		material({
-			emissivity: value(1, 1, 1)
-		}))
-]));
+createViewer(document.body, scene({
+	camera: mouseCamera({
+		distance: value(4)
+	}),
+	entities: [
+		entity(
+			unitSphere(),
+			material({
+				color: value(0.8, 0.8, 1)
+			})),
+		entity(
+			translate(value(0, 4, 0),
+				scale(value(5), unitBox())),
+			material({
+				color: value(0.8, 0.8, 0.8)
+			})),
+		entity(
+			plane(value(0, -1, 0), value(5)),
+			material({
+				emissivity: value(1, 1, 1)
+			}))
+	]
+}));
 
 
