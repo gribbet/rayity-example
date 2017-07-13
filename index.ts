3f428ffc2ef7529f8f17abdca50ec495a2f5bc7f
Graham <graham.gibbons@gmail.com>
2017-07-13 15:14:48 -0700

    Save

--- index.ts
+++ index.ts
@@ -12,59 +12,54 @@ import {
 	unitBox,
 	unitCylinder,
 	unitSphere,
-	value, sierpinski, rotateX, twistZ, rotateY, rotateZ, repeat, wrapX, twistY, Shape, torus, union
+	value, sierpinski, rotateX, blend, twistZ, rotateY, expand, stretch, rotate, rotateZ, repeat, wrapX, twistY, torus, union, mirror, Expression, Shape, spotlightMaterial
 } from "traymarch";
 
 createViewer(document.body, scene({
 	camera: mouseCamera({
 		fieldOfView: value(60 / 180 * Math.PI),
 		distance: value(20),
-		aperture: value(0.1)
+		aperture: value(0.2)
 	}),
 	models: [
 		model(
-			scale(value(1000),
-				unitSphere()),
-			material({
-				emissivity: value(2, 2, 2)
+			scale(value(1000), unitSphere()),
+			spotlightMaterial({
+				color: value(0.5),
+				direction: value(1, 1, 0),
+				spread: value(0.01),
+				ambient: value(0.5, 0.6, 0.9)
 			})),
 		model(
-			scale(value(2),
-				unitSphere()),
+			plane(value(0, 1, 0), value(3)),
 			material({
-				emissivity: value(200, 200, 200)
+				color: value(0.9)
 			})),
 		model(
-			rotateY(value(0.7),
-				rotateZ(value(0.4),
-					rotateX(value(0.2),
-						repeat(value(2, 2, 2),
-							intersection(
-								union(
-									union(
-										translate(value(1, 0, 1),
-											torus()),
-										translate(value(-1, 1, 0),
-											rotateX(value(Math.PI / 2),
-												torus()))),
-									translate(value(0, -1, -1),
-										rotateZ(value(Math.PI / 2),
-											torus()))),
-								unitBox()))))),
+			repeat(value(10, 0, 10),
+				mirror(value(1, 1, 0),
+					mirror(value(1, 0, 1),
+						mirror(value(0, 1, 1),
+							rotate(
+								value(1, 0, -1),
+								value(Math.atan(Math.sqrt(2))),
+								blend(
+									value(2.2),
+									translate(value(0, 4, 0),
+										unitSphere()),
+									translate(value(0, 1, 0),
+										unitBox()))))))),
 			material({
-				transmittance: value(0.99),
-				scatter: value(0.02),
-				refraction: value(2),
 				smoothness: value(0.9),
-				color: value(0.5, 0.8, 0.9)
+				color: value(0.8, 0.8, 0.7)
 			}))
 	]
 }), {
-		width: 128,
-		height: 128,
+		width: 256,
+		height: 256,
 		epsilon: 1e-4,
-		steps: 150,
-		bounces: 40,
-		iterations: 4
+		steps: 100,
+		bounces: 8,
+		iterations: 2
 	});
 
