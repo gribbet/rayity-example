4c317e9ffb44999ab102762fdce271c23c0e2774
Graham <graham.gibbons@gmail.com>
2017-07-18 15:54:21 -0700

    Update

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import {
 	intersection, mandelbulb,
 	material,
 	model,
-	mouseCamera,
+	orbit,
 	plane,
 	scale,
 	scene,
@@ -12,54 +12,62 @@ import {
 	unitBox,
 	unitCylinder,
 	unitSphere,
-	value, sierpinski, rotateX, blend, twistZ, rotateY, expand, stretch, rotate, rotateZ, repeat, wrapX, twistY, torus, union, mirror, Expression, Shape, spotlightMaterial
+	value, sierpinski, rotateX, blend, offset, twistZ, rotateY, expand, stretch, rotate, rotateZ, repeat, wrapX, twistY, torus, union, mirror, Expression, Shape, spotlight
 } from "traymarch";
 
 createViewer(document.body, scene({
-	camera: mouseCamera({
-		fieldOfView: value(60 / 180 * Math.PI),
-		distance: value(20),
-		aperture: value(0.2)
+	air: material({
+		scatter: value(1000)
+	}),
+	camera: orbit({
+		fieldOfView: value(90 / 180 * Math.PI),
+		distance: value(10),
+		aperture: value(0.05),
+		offset: `vec2(time * 0.05, 0)`
 	}),
 	models: [
-		model(
-			scale(value(1000), unitSphere()),
-			spotlightMaterial({
-				color: value(0.5),
-				direction: value(1, 1, 0),
+		model({
+			shape: scale(value(1000),
+				unitSphere()),
+			material: spotlight({
+				color: value(0.1),
+				direction: value(0, 1, -1),
 				spread: value(0.01),
 				ambient: value(0.5, 0.6, 0.9)
-			})),
-		model(
-			plane(value(0, 1, 0), value(3)),
-			material({
-				color: value(0.9)
-			})),
-		model(
-			repeat(value(10, 0, 10),
-				mirror(value(1, 1, 0),
-					mirror(value(1, 0, 1),
-						mirror(value(0, 1, 1),
-							rotate(
-								value(1, 0, -1),
-								value(Math.atan(Math.sqrt(2))),
-								blend(
-									value(2.2),
-									translate(value(0, 4, 0),
-										unitSphere()),
-									translate(value(0, 1, 0),
-										unitBox()))))))),
-			material({
-				smoothness: value(0.9),
-				color: value(0.8, 0.8, 0.7)
-			}))
+			})
+		}),
+		model({
+			shape: plane(value(0, 1, 0), value(3.2)),
+			material: material({
+				color: value(0.8)
+			})
+		}),
+		model({
+			shape: mirror(value(1, 1, 0),
+				mirror(value(1, 0, 1),
+					mirror(value(0, 1, 1),
+						rotate(
+							value(1, 0, -1),
+							value(Math.atan(Math.sqrt(2))),
+							blend(
+								value(2.2),
+								translate(value(0, 4, 0),
+									unitSphere()),
+								translate(value(0, 1, 0),
+									unitBox())))))),
+			material: material({
+				color: value(0.9, 0.5, 0.5),
+				smoothness: value(0.99)
+			})
+		})
 	]
 }), {
 		width: 256,
 		height: 256,
-		epsilon: 1e-4,
+		epsilon: 1e-3,
 		steps: 100,
-		bounces: 8,
-		iterations: 2
+		bounces: 30,
+		iterations: 4,
+		memory: 0.95
 	});
 
