d987a350bd005dc150b00a69305f0a1cab6a362d
Graham <graham.gibbons@gmail.com>
2017-08-08 15:15:35 -0700

    Noisey boxes

--- index.ts
+++ index.ts
@@ -1,19 +1,22 @@
 import {
     camera,
     createViewer,
-    difference,
+    expression,
+    intersection,
     material,
     model,
+    modulate,
+    options,
     orbit,
     plane,
-    repeat,
+    random,
     scale,
     scene,
+    smoothBox,
+    sphere,
     spotlight,
-    unitSphere,
-	value,
-	options,
-} from 'traymarch';
+    value,
+} from 'traymarch/lib';
 
 
 createViewer(
@@ -21,50 +24,45 @@ createViewer(
 	scene({
 		air: material({
 			scatter: value(1000),
+			//emissivity: value(0.1, 0.1, 0.1)
 		}),
 		camera: orbit({
 			fieldOfView: value(60 / 180 * Math.PI),
-			distance: value(50),
-			aperture: value(0.05),
-			target: value(0),
-			offset: value(0.25, 0.1)
+			distance: value(10),
+			aperture: value(0.1),
+			target: value(0, 0, 0),
+			offset: value(0.25, -0.5)
 		}),
 		models: [
 			model({
 				shape: scale(value(1000),
-					unitSphere()),
+					sphere()),
 				material: spotlight({
-					color: value(0.3),
+					color: value(1),
 					direction: value(1, 1, 1),
-					spread: value(0.01),
-					ambient: value(0.5, 0.6, 0.9)
+					spread: value(0.1),
+					ambient: value(0)
 				})
 			}),
 			model({
-				shape: plane(value(0, 1, 0), value(0.5)),
+				shape: intersection(
+					modulate(value(1, 100, 1), index =>
+						smoothBox(expression(`0.9, 1.0 + 5.0 * ${random(index)}.x, 0.9`), value(0.5))),
+					plane(value(0, 1, 0), value(-6))),
 				material: material({
-					color: value(0.1),
-					smoothness: value(0.8)
-				})
-			}),
-			model({
-				shape: repeat(value(4, 0, 1),
-					difference(
-						unitSphere(),
-						scale(value(0.9),
-							unitSphere()))),
-				material: material({
-					smoothness: value(0),
+					color: value(0.7, 0.6, 0.5),
+					smoothness: value(0.99)
 				})
 			})
 		]
 	}), options({
-		width: 512,
+		width: 512, 
 		height: 512,
-		epsilon: 1e-5,
+		epsilon: 1e-4,
 		steps: 200,
-		bounces: 10,
+		bounces: 6,
 		iterations: 1,
-		cheapNormals: false
+		cheapNormals: true,
+		memory: 1
 	}));
 
