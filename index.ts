e4aad5494f346dc9ab07cc8ceae4c039adf711cd
Graham <graham.gibbons@gmail.com>
2017-08-24 10:05:21 -0700

    Reset basic example

--- index.ts
+++ index.ts
@@ -1,22 +1,18 @@
 import {
-    camera,
-    createViewer,
-    expression,
-    intersection,
-    material,
-    model,
-    modulate,
-    options,
-    orbit,
-    plane,
-    random,
-    scale,
-    scene,
-    smoothBox,
-    sphere,
-    spotlight,
-    value,
-} from 'traymarch/lib';
+	camera,
+	createViewer,
+	dodecahedron,
+	material,
+	model,
+	options,
+	orbit,
+	plane,
+	scale,
+	scene,
+	sphere,
+	spotlight,
+	value,
+} from 'rayity';
 
 
 createViewer(
@@ -24,13 +20,12 @@ createViewer(
 	scene({
 		air: material({
 			scatter: value(1000),
-			//emissivity: value(0.1, 0.1, 0.1)
 		}),
 		camera: orbit({
 			fieldOfView: value(60 / 180 * Math.PI),
-			distance: value(10),
+			distance: value(2),
 			aperture: value(0.1),
-			target: value(0, 0, 0),
+			target: value(0),
 			offset: value(0.25, -0.5)
 		}),
 		models: [
@@ -40,27 +35,29 @@ createViewer(
 				material: spotlight({
 					color: value(1),
 					direction: value(1, 1, 1),
-					spread: value(0.1),
+					spread: value(0.05),
 					ambient: value(0)
 				})
 			}),
 			model({
-				shape: intersection(
-					modulate(value(1, 100, 1), index =>
-						smoothBox(expression(`0.9, 1.0 + 5.0 * ${random(index)}.x, 0.9`), value(0.5))),
-					plane(value(0, 1, 0), value(-6))),
+				shape: plane(value(0, 1, 0), value(0.5)),
 				material: material({
-					color: value(0.7, 0.6, 0.5),
-					smoothness: value(0.99)
+					color: value(0.5)
+				})
+			}),
+			model({
+				shape: dodecahedron(),
+				material: material({
+					color: value(0.7, 0.6, 0.5)
 				})
 			})
 		]
 	}), options({
-		width: 512, 
+		width: 512,
 		height: 512,
 		epsilon: 1e-4,
-		steps: 200,
-		bounces: 6,
+		steps: 100,
+		bounces: 5,
 		iterations: 1,
 		cheapNormals: true,
 		memory: 1
