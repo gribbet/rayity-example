5d0bb736e1619cfbea78bbf48469239dc943247c
Graham <graham.gibbons@gmail.com>
2017-09-14 09:16:32 -0700

    Reset example

--- index.ts
+++ index.ts
@@ -1,65 +1,36 @@
-import {
-	camera,
-	viewer,
-	expression,
-	material,
-	model,
-	options,
-	orbit,
-	plane,
-	repeat,
-	scale,
-	scene,
-	skull,
-	smoothBox,
-	sphere,
-	spotlight,
-	translate, rotateZ,
-	value, cube
-} from 'rayity/lib';
+import { camera, cube, material, model, options, orbit, plane, scene, sphere, translate, value, viewer } from 'rayity/lib';
 
 viewer(
 	document.body,
 	scene({
-		air: material({
-			scatter: value(50000)
-		}),
 		camera: orbit({
-			fieldOfView: value(60 / 180 * Math.PI),
-			radius: value(1.1),
-			aperture: value(0.02),
-			target: value(0),
-			offset: value(-0.35, -0.2),
-			focalFactor: value(0.75)
+			radius: value(4),
+			offset: value(0.25, -0.5)
 		}),
 		models: [
 			model({
-				shape: scale(value(0.2), sphere()),
+				shape: plane(value(0, 1, 0), value(0.5)),
 				material: material({
-					emissivity: value(2)
+					color: value(0.6)
 				})
 			}),
 			model({
-				shape: rotateZ(value(-Math.PI / 4),
-					skull()),
+				shape: translate(value(-0.5, 0, 0),
+					sphere()),
 				material: material({
-					color: value(0.899, 0.898, 0.897),
-					transmittance: value(0.99),
-					smoothness: value(0.5),
-					refraction: value(1.8),
+					color: value(0.8, 0.4, 0.8)
+				})
+			}),
+			model({
+				shape: translate(value(0.5, 0, 0),
+					cube()),
+				material: material({
+					color: value(0.8, 0.9, 0.1)
 				})
 			})
 		]
 	}), options({
 		width: 512,
 		height: 512,
-		epsilon: 1e-5,
-		steps: 80,
-		bounces: 20,
-		iterations: 1,
-		cheapNormals: false,
-		memory: 1.0,
-		stepFactor: 0.6,
-		gamma: 3
 	}));
 
